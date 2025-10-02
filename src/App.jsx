417c8bc24142fd62f7ad3626e715c43e28d03859
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-02 16:01:47 -0400

    Changed my approach, trying to work backwards from lecture example

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,67 @@ import './App.css'
 
 // Components
 import Header from './components/Header'
-import AddTask from './components/AddTask'
+import TextInput from './components/TextInput';
+import ActionButton from './components/ActionButton';
+import PartyMember from './components/PartyMember';
+import partyReducer from './reducers/partyReducer'
+
 
 
 
 // List item basic values:
 // {
-//   id: Number ( for filtering/deleting),
-//   todo: String,
-//   completed: Boolean,
+//   id: Number( for filtering / deleting),
+//     todo: String,
+//       completed: Boolean,
 // }
 
 
 function App() {
-  const [task, setTask] = useState("");
+  const [todo, setToDo] = useState("");
+  const [party, dispatch] = useReducer(partyReducer, initialState);
+
+
+  const partyList = party.map((li) => {
+    return (
+      <PartyMember key={li.todo} li={li} dispatch={dispatch} />
+
+    )
+  })
+
 
   return (
     <>
       <Header />
-      <AddTask state={task} setState={setTask}/>
-    
+      <div>
+        <TextInput state={todo} setState={setToDo} />
+        <ActionButton
+          type="addToDo"
+          payload={{ todo }}
+          dispatch={dispatch}
+        >
+          Add
+        </ActionButton>
+      </div>
+      <div>
+        {partyList}
+      </div>
     </>
-  )
+  );
 }
+
+const initialState = [
+  {
+    id: 1,
+    todo: "Clean apartment",
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: "Buy Groceries",
+    completed: false,
+  }
+];
+
 
 export default App
---
+++ src/components/PartyMember.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ActionButton from "./ActionButton";
+
+
+export default function PartyMember({ li, dispatch }) {
+  
+
+  return (
+    <>
+      <h2>{li.todo}</h2>
+      <div style={{ width: "100%", textAlign: "center", marginBottom: "25px" }}>
+        <ActionButton
+          type="edit"
+          payload={{ id: li.id }}
+          dispatch={dispatch}
+          className="one-character"
+        >
+          Edit
+        </ActionButton>
+        <ActionButton
+          type="delete"
+          payload={{ id: li.id }}
+          dispatch={dispatch}
+          className="one-character"
+        >
+          Delete
+        </ActionButton>
+      </div>
+    </>
+  );
+}
