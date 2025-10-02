34dccaedb320372512fbc781af9758a2f14ce271
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-02 14:01:21 -0400

    First Commit, Opened React, cleaned out app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+// List item basic values:
+// {
+//   id: Number ( for filtering/deleting),
+//   todo: String,
+//   completed: Boolean,
+// }
+
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
