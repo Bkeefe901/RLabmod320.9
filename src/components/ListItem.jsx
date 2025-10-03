de295ae1db4af0be2f04750b493a46380d71f5bb
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-03 15:03:06 -0400

    Started working on reducer function. Adding to list capability

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,12 +3,19 @@ import ActionButton from "./ActionButton";
 
 
 export default function ListItem({ li, dispatch }) {
+
+
+  function handleChange(e){
+    return !checked;
+  }
   
 
   return (
     <>
-      <h2>{li.todo}</h2>
-      <div style={{ width: "100%", textAlign: "center", marginBottom: "25px" }}>
+      
+      <div style={{ width: "100%", textAlign: "center", marginBottom: "25px", display: "flex" }}>
+        <input type="checkbox" id={li.name}  onChange={handleChange} />
+        <h2>{li.todo}</h2>
         <ActionButton
           type="edit"
           payload={{ id: li.id }}
@@ -29,3 +36,6 @@ export default function ListItem({ li, dispatch }) {
     </>
   );
 }
+
+
+// checked={li.completed}
--- src/reducers/listReducer.jsx
+++ src/reducers/listReducer.jsx
@@ -1,62 +1,103 @@
-export default function reducer(state, { type, payload: { name, step } }) {
+export default function reducer(state, { type, payload: { todo, id } }) {
   switch (type) {
-    case "add_adventurer": {
-      if (name === "") {
+    case "addToDo": {
+      if (todo === ""){
         return state;
       }
 
-      let hasThisName = false;
+      let toDoExists = false;
       state.forEach((e) => {
-        if (e.name === name) {
+        if(e.todo == todo){
           alert(
-            `There is already a ${name} in your party.
-Differentiate them somehow!`
-          );
-          hasThisName = true;
+            `The todo: ${todo} is already in your list`
+          )
+          toDoExists = true;
         }
       });
+      //let i = state.length;
 
-      if (hasThisName) return state;
-      return [{ name: name, health: 100 }, ...state];
+      if (toDoExists) return state;
+      return [{
+        todo,
+        completed:false
+       }, 
+      ...state
+      ]
     }
-    case "remove_adventurer": {
-      return state.filter((s) => s.name !== name);
-    }
-    case "increment_health": {
-      return state.map((s) => {
-        if (s.name === name) {
-          let newHealth = s.health + step;
-          newHealth = newHealth > 100 ? 100 : newHealth;
-
-          return {
-            ...s,
-            health: newHealth
-          };
-        } else return s;
-      });
-    }
-    case "decrement_health": {
-      const newState = state.map((s) => {
-        if (s.name === name) {
-          let newHealth = s.health - step;
-          newHealth = newHealth < 0 ? 0 : newHealth;
-
-          return {
-            ...s,
-            health: newHealth
-          };
-        } else return s;
-      });
-
-      const filteredNewState = newState.filter((s) => s.health !== 0);
-      if (newState.length !== filteredNewState.length) {
-        alert(`${name} has retired from adventuring...`);
-      }
-
-      return filteredNewState;
+    case "delete": {
+      
     }
     default: {
-      throw Error("Unknown Action: " + type);
+      throw new Error();
     }
   }
 }
+
+
+
+
+
+
+
+// export default function reducer(state, { type, payload: { name, step } }) {
+//   switch (type) {
+//     case "add_adventurer": {
+//       if (name === "") {
+//         return state;
+//       }
+
+//       let hasThisName = false;
+//       state.forEach((e) => {
+//         if (e.name === name) {
+//           alert(
+//             `There is already a ${name} in your party.
+// Differentiate them somehow!`
+//           );
+//           hasThisName = true;
+//         }
+//       });
+
+//       if (hasThisName) return state;
+//       return [{ name: name, health: 100 }, ...state];
+//     }
+//     case "remove_adventurer": {
+//       return state.filter((s) => s.name !== name);
+//     }
+//     case "increment_health": {
+//       return state.map((s) => {
+//         if (s.name === name) {
+//           let newHealth = s.health + step;
+//           newHealth = newHealth > 100 ? 100 : newHealth;
+
+//           return {
+//             ...s,
+//             health: newHealth
+//           };
+//         } else return s;
+//       });
+//     }
+//     case "decrement_health": {
+//       const newState = state.map((s) => {
+//         if (s.name === name) {
+//           let newHealth = s.health - step;
+//           newHealth = newHealth < 0 ? 0 : newHealth;
+
+//           return {
+//             ...s,
+//             health: newHealth
+//           };
+//         } else return s;
+//       });
+
+//       const filteredNewState = newState.filter((s) => s.health !== 0);
+//       if (newState.length !== filteredNewState.length) {
+//         alert(`${name} has retired from adventuring...`);
+//       }
+
+//       return filteredNewState;
+//     }
+//     default: {
+//       throw Error("Unknown Action: " + type);
+//     }
+//   }
+// }
