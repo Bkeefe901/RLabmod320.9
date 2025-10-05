c819e30e4a05f6619fbdc6c1ae6823e933e44dda
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-05 19:58:21 -0400

    Everything working, just need to clean up and try to optimize

--- src/components/EditField.jsx
+++ src/components/EditField.jsx
@@ -3,13 +3,13 @@ import TextInput from "./TextInput";
 import { useState } from "react";
 
 export default function EditField({li, dispatch}) {
-    const [save, setSave] = useState("");
+    const [save, setSave] = useState(li.todo);
 
 
     return (
         <div>
             <input type="checkbox" id={li.id} />
-            <TextInput state={save} setState={setSave} placeHolder={li.todo}/>
+            <TextInput state={save} setState={setSave} />
             <ActionButton
                 type="save"
                 payload={{id: li.id, todo: li.todo, save}}
